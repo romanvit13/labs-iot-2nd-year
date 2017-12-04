326adceea338b4b70355d26e7821398b9fc9316c
romanvit13 <romanvit13@gmail.com>
2017-12-04 23:33:59 +0200

    [+] add indexedDB news add and read

--- Web/Site/js/admin.js
+++ Web/Site/js/admin.js
@@ -1,5 +1,5 @@
 var index = 0;
-
+useLocalStorage = false;
 function isOnline() {
     return window.navigator.onLine;
 }
@@ -10,20 +10,32 @@ function newsAdd() {
         ($('#news-text').val() === "")) {
         alert('Деякі дані не заповнені');
         return false;
+    }
+    if (isOnline()) {
+        $('#news-name').val('');
+        $('#news-text').val('');
+        alert('Новину успішно додано');
     } else {
-        if (isOnline()) {
-            $('#news-name').val('');
-            $('#news-text').val('');
-            alert('Новину успішно додано');
-        } else {
+        if (useLocalStorage) {
             var caption = document.getElementById('caption').value;
             var text = document.getElementById('text').value;
-            index ++;
+            index++;
             var objects = [];
-            objects.push({"caption":caption,"text":text});
-            localStorage.setItem(index , JSON.stringify(objects));
+            objects.push({"caption": caption, "text": text});
+            localStorage.setItem(index, JSON.stringify(objects));
             document.getElementById('newsForm').reset();
             alert("Відсутній зв'зязок, збережено в локальному сховищі.");
         }
+        else {
+            var transaction = db.transaction(["news"], "readwrite");
+            var store = transaction.objectStore("news");
+            var news1 = {
+                caption: document.getElementById('caption').value,
+                text: document.getElementById('text').value
+            };
+            store.add(news1);
+        }
+        document.getElementById('newsForm').reset();
     }
+
 }
--- Web/Site/js/news.js
+++ Web/Site/js/news.js
@@ -10,23 +10,52 @@ window.addEventListener('load', function () {
     window.addEventListener('offline', updateOnlineStatus);
 });
 
+useLocalStorage = false;
+
 function isOnline() {
     return window.navigator.onLine;
 }
 
 
 function getNews() {
-    leng = localStorage.length+1;
-    for (var i = 1; i < leng; i++){
-        news = JSON.parse(localStorage.getItem(i));
-        var parentElem = document.getElementById('newsList');
-        var out = document.createElement('div');
-        out.id = 'news';
-        out.innerHTML =
-            "<div class='col-md-4' ><div class='thumbnail'> <a href='#'> <img src='images/news3.jpg'>" +
-            "<div class='caption'><p>" + news[0].caption +"</p>" +
-            "</div> <p>"+ news[0].text+"</p> </a></div></div>";
-        parentElem.appendChild(out);
-        localStorage.removeItem(i);
+    if (useLocalStorage) {
+        leng = localStorage.length + 1;
+        for (var i = 1; i < leng; i++) {
+            news = JSON.parse(localStorage.getItem(i));
+            var parentElem = document.getElementById('newsList');
+            var out = document.createElement('div');
+            out.id = 'news';
+            out.innerHTML =
+                "<div class='col-md-4' ><div class='thumbnail'> <a href='#'> <img src='images/news3.jpg'>" +
+                "<div class='caption'><p>" + news[0].caption + "</p>" +
+                "</div> <p>" + news[0].text + "</p> </a></div></div>";
+            parentElem.appendChild(out);
+            localStorage.removeItem(i);
+        }
+    } else {
+        var transaction = db.transaction(["news"], "readonly");
+        var store = transaction.objectStore("news");
+        store.openCursor().onsuccess = function (e) {
+            var cursor = e.target.result;
+            if (cursor) {
+                cursor.continue();
+                var parentElem = document.getElementById('newsList');
+                var out = document.createElement('div');
+                out.id = 'news';
+                // out.innerHTML =
+                //     "<div class='col-md-4'>" +
+                // "<div class='card'> " +
+                // "<a href='#'> " +
+                // "<img src='" + cursor.value.img + "' width='100%'>" +
+                // "<div class='caption'><p>" + cursor.value.name + "</p></div>" +
+                // " <p>" + cursor.value.text + "</p>" +
+                // "</a></div></div>";
+                out.innerHTML =
+                    "<div class='col-md-4' ><div class='thumbnail'> <a href='#'> <img src='images/news3.jpg'>" +
+                    "<div class='caption'><p>" + cursor.value.caption + "</p>" +
+                    "</div> <p>" + cursor.value.text + "</p> </a></div></div>";
+                parentElem.appendChild(out);
+            }
+        }
     }
 }
